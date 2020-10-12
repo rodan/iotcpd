dd3f7f22b7cd85d14152e15261eee28536f46da9
Petre Rodan <c59ccef70aa1387612c4@subdimension.ro>
2020-10-12 07:21:46 +0300

    fix shellcheck warnings

--- server/test_adblock.sh
+++ server/test_adblock.sh
@@ -3,19 +3,19 @@
 count=1
 
 if [ "$1" == "a" ]; then
-    for ((i=0;i<${count};i++)); do
+    for ((i=0;i<count;i++)); do
         echo 'http://www.wowomg.com/foo.html 127.0.1.1/10.129.23.1 - GET myip=- myport=3127' | nc 127.0.0.1 9991 2>/dev/null &
     done
 elif [ "$1" == "b" ]; then
-   for ((i=0;i<${count};i++)); do
+   for ((i=0;i<count;i++)); do
         echo "http://clean_url/baz/${RANDOM}-${i} 127.0.1.1/10.129.23.1 - GET myip=- myport=3127" | nc 127.0.0.1 9991 2>/dev/null &
     done
 elif [ "$1" == "c" ]; then
-   for ((i=0;i<${count};i++)); do
+   for ((i=0;i<count;i++)); do
         echo "http://bar.baaz.foo/test.html 127.0.1.1/10.129.23.1 - GET myip=- myport=3127" | nc 127.0.0.1 9991 2>/dev/null &
     done
 elif [ "$1" == "d" ]; then
-    for ((i=0;i<${count};i++)); do
+    for ((i=0;i<count;i++)); do
         echo "http://d1fjasmrhc6q3w.cloudfront.net/testImages/testghostie_ad.jpg 127.0.1.1/10.129.23.1 - GET myip=- myport=3127" | nc 127.0.0.1 9991 2>/dev/null &
     done
     exit 0
