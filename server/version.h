1d67aff367a5c788a370e3d2c0154a51c9d94d7e
Petre Rodan <2b4eda@subdimension.ro>
2021-10-26 18:40:20 +0000

    v1.1

--- server/version.h
+++ server/version.h
@@ -1,11 +1,11 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-// compiled on Tue Oct 26 14:14:29 UTC 2021
+// compiled on Tue Oct 26 18:38:44 UTC 2021
 
 #define VER_MAJOR   1
-#define VER_MINOR   0
-#define BUILD 102
-#define COMMIT 38
+#define VER_MINOR   1
+#define BUILD 103
+#define COMMIT 44
 
 #endif
